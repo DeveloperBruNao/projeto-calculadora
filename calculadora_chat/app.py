09e99a6ce87511ed305034a063e69f4cf41df33b
Bruno Damasceno DevRPA <eubruno.rpa@gmail.com>
2024-11-21 15:18:45 -0300

    Cria funcionalidade de cálculo com Flask ao enviar formulário

--- calculadora_chat/app.py
+++ calculadora_chat/app.py
@@ -5,3 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/calcular', methods=['POST'])
+def calcular():
+    if request.method == 'POST':
+        expressao = request.form['expressao']
+        try:
+            resultado = eval(expressao)
+        except Exception as e:
+            resultado = "Erro" + str(e)
+        return render_template('index.html', resultado=resultado, expressao=expressao)
+
+if __name__ == '__main__':
+    app.run(debug=True)
